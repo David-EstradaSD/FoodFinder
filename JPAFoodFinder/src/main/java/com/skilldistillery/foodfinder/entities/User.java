babbe9df54fba84eea8962e8d5cb2335986edd20
Shaun R <shaun.reass@icloud.com>
2021-08-17 16:28:23 -0500

    j unit test green

--- JPAFoodFinder/src/main/java/com/skilldistillery/foodfinder/entities/User.java
+++ JPAFoodFinder/src/main/java/com/skilldistillery/foodfinder/entities/User.java
@@ -12,4 +12,53 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	private String username;
+	
+	
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 }
--- JPAFoodFinder/src/test/java/com/skilldistillery/foodfinder/entities/UserTest.java
+++ JPAFoodFinder/src/test/java/com/skilldistillery/foodfinder/entities/UserTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.foodfinder.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +15,36 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em; 
+	private User user;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAFoodFinder");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager(); 
+		user = em.find(User.class, 1); 
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null; 
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(user);
+		assertEquals("hello", user.getUsername()); 
 	}
 
 }
