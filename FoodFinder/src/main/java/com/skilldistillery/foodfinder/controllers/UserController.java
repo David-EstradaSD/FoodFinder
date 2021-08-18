aa0f3fe2e20e6fb60c214d86bb9a921be0558b93
Shaun R <shaun.reass@icloud.com>
2021-08-17 21:52:26 -0500

    user controller import completed

--- FoodFinder/src/main/java/com/skilldistillery/foodfinder/controllers/UserController.java
+++ FoodFinder/src/main/java/com/skilldistillery/foodfinder/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.foodfinder.controllers;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
