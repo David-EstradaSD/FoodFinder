291db4f4bb806b8573ee4c4b6dcf3b2d4edc86ad
David <destrada2193@gmail.com>
2021-08-19 10:45:09 -0700

    Permit public access to index for service locations

--- FoodFinder/src/main/java/com/skilldistillery/foodfinder/security/SecurityConfig.java
+++ FoodFinder/src/main/java/com/skilldistillery/foodfinder/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // For CORS, the preflight request
         .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()     // will hit the OPTIONS on the route
  //       .antMatchers(HttpMethod.GET, "/api/todos").hasRole("admin") // Can access when admin is in their role
+        .antMatchers(HttpMethod.GET, "/api/service-locations").permitAll() // This ALLOWS the specified path to be permitted to access without authentication (aka to the general public)
         .antMatchers("/api/**").authenticated() // Requests for our REST API must be authorized.
         .anyRequest().permitAll()               // All other requests are allowed without authorization.
         .and()
