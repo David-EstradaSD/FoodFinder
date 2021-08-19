b3ee0cca4bb939542f159ea24ed48cec4cd21675
David <destrada2193@gmail.com>
2021-08-19 10:33:18 -0700

    Complete ServiceLocation repo, service, serviceImpl classes, controller, test with Postman success

---
+++ FoodFinder/src/main/java/com/skilldistillery/foodfinder/repositories/ServiceLocationRepository.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.foodfinder.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.foodfinder.entities.ServiceLocation;
+
+public interface ServiceLocationRepository extends JpaRepository<ServiceLocation, Integer> {
+
+	List<ServiceLocation> findByUser_Username(String username);
+	
+	ServiceLocation findByIdAndUser_Username(int slId, String username);
+
+	
+}
