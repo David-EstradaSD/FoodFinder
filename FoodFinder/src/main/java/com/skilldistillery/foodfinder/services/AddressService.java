0067438a00aa1aabed4602901399e454d1b49ab2
Shaun R <shaun.reass@icloud.com>
2021-08-20 15:28:56 -0500

    donor and recipient create completed

--- FoodFinder/src/main/java/com/skilldistillery/foodfinder/services/AddressService.java
+++ FoodFinder/src/main/java/com/skilldistillery/foodfinder/services/AddressService.java
@@ -7,4 +7,10 @@ import com.skilldistillery.foodfinder.entities.Address;
 public interface AddressService {
 	
 	public List<Address> index(); 
+	
+	public Address donorCreate(Address address, String username); 
+	
+	public Address recipientCreate(Address address, String username); 
+	
+	public Address update(); 
 }
