4fcaa29862f11e4573facbb9176f8e47d6b57308
David <destrada2193@gmail.com>
2021-08-21 08:57:42 -0700

    Complete Donor service methods and controller

--- FoodFinder/src/main/java/com/skilldistillery/foodfinder/services/AddressService.java
+++ FoodFinder/src/main/java/com/skilldistillery/foodfinder/services/AddressService.java
@@ -11,9 +11,8 @@ public interface AddressService {
 	public Address donorCreate(Address address, String username); 
 	
 	public Address recipientCreate(Address address, String username); 
+	
 	public Address create(Address address); 
-	
-	
 	
 	public Address donorUpdate(Address address, String username, int addressId); 
 	
