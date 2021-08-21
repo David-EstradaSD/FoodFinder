82fc024afc8ce1e7c3e6ef3d868cd9e3dbfa34df
Shaun R <shaun.reass@icloud.com>
2021-08-21 17:49:06 -0500

    updated comment controller

---
+++ FoodFinder/src/main/java/com/skilldistillery/foodfinder/services/CommentService.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.foodfinder.services;
+
+import java.util.List;
+
+import com.skilldistillery.foodfinder.entities.Comment;
+
+public interface CommentService {
+
+	public List<Comment> index(String locationName);
+
+	public Comment create(String username, Comment comment, int locationId);
+	
+	public Comment update(String username, Comment comment, int locationId, int commentId);
+	
+	public boolean delete(String username, int commentId);
+
+}
