7ff52bc1868ae56cad3e49bac264040d50ae34bc
David <destrada2193@gmail.com>
2021-08-22 11:58:29 -0700

    Complete address, comment, rating and service models in ngFood

--- ngFoodfinder/src/app/models/address.ts
+++ ngFoodfinder/src/app/models/address.ts
@@ -1,2 +1,23 @@
 export class Address {
+
+  id : number;
+  streetAddress : string;
+  city : string;
+  state : string;
+  zip : string;
+
+  constructor(
+    id : number = 0,
+    streetAddress : string = '',
+    city : string = '',
+    state : string = '',
+    zip : string = ''
+  ) {
+    this.id = id;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+  }
+
 }
